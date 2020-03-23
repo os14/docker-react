6e6ce05cd9109291d29e1c92e5631c40fc1a99c9
Omer Slik <omers1414@gmail.com>
2020-03-23 10:57:52 +0200

    changed feature

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactwefrwesdsd
+          Learn Reactwefrwesdsd chamged feature
         </a>
       </header>
     </div>
